40f4d9ba15eefce90bad489fdc4697b398bfb946
Andrey <andreymaarques@hotmail.com>
2025-07-08 15:36:54 -0300

    Iniciando Projeto

---
+++ lib/pages/todo_list_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget {
+  TodoListPage({super.key});
+  final TextEditingController emailController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(controller: emailController, decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Email'), onChanged: onChanged),
+              ElevatedButton(onPressed: login, child: Text('Entrar')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void login() {
+    String email;
+    email = emailController.text;
+    // ignore: avoid_print
+    print(email);
+    emailController.clear();
+  }
+
+  void onChanged(String text) {
+    print(text);
+  }
+}
